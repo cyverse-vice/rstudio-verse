acf077b020d7444787b8ae3161b19c14408f228b
Ryan Bartelme <r.p.bartelme@gmail.com>
2021-02-24 15:36:57 -0700

    Update run.sh

--- 4.0.0-ubuntu18.04/run.sh
+++ 4.0.0-ubuntu18.04/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-chown -R rstudio:rstudio /home/rstudio
+#chown -R rstudio:rstudio /home/rstudio
 gomplate -f /nginx.conf.tmpl -o /etc/nginx/nginx.conf
 supervisord -c /etc/supervisor/supervisord.conf -n
